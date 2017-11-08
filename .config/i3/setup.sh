5a3b9e4653da42761e613657f1b2da2b64967588
Christopher Peterson <3893680+cspeterson@users.noreply.github.com>
2017-11-08 09:32:31 -0500

    Replace notify-osd with dunst as part of i3 setup

--- .config/i3/setup.sh
+++ .config/i3/setup.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Install tools and applets upon which the i3 config will rely..."
+set -e
+
+echo "::::Replace notify-osd with dunst..."
+sudo apt-get remove notify-osd
+sudo killall notify-osd || true
+sudo apt-get install dunst
+notify-send "dunst test" "Are we dunst yet?"
+
+echo "::::Install tools and applets upon which the i3 config will rely..."
 sudo apt-get install \
 blueman \
 fonts-font-awesome \
@@ -9,14 +17,13 @@ gsimplecal \
 i3blocks \
 i3lock \
 libimage-exiftool-perl \
-notify-osd \
 pasystray \
 pavucontrol \
 pcmanfm-qt \
 shutter
 
-echo 'Pip install the i3ipc module for this user...'
+echo '::::Pip install the i3ipc module for this user...'
 pip3 install --user --upgrade i3ipc
-echo 'Download focus-next-visible.py from the i3ipc-python github repo...'
+echo '::::Download focus-next-visible.py from the i3ipc-python github repo...'
 wget -O ~/.config/i3/focus-next-visible.py https://raw.githubusercontent.com/acrisci/i3ipc-python/master/examples/focus-next-visible.py
 ls -l ~/.config/i3/focus-next-visible.py
