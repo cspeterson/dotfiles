3c57dcf7abc7f7c6c616f5433461cf92513d4afa
Christopher Peterson <3893680+cspeterson@users.noreply.github.com>
2017-11-17 08:46:56 -0500

    Fix audacity setup script, stop tracking individual plugins

--- .audacity-files/setup.sh
+++ .audacity-files/setup.sh
@@ -7,11 +7,11 @@ plugins=(
 echo "Installing Audacity plugins if not installed..."
 for plugin in ${plugins[@]}; do
 	filename=$(basename "${plugin}")
-  plugindir="~/.audacity-files/plug-ins"
+	plugindir="~/.audacity-files/plug-ins"
 	filepath="${plugindir}/${filename}"
 	if [ ! -f "${filepath}" ]; then
 		echo "Installing plugin ${filename}..."
-    mkdir -p "${plugindir}"
+		mkdir -p "${plugindir}"
 		wget -O "${filepath}" "${plugin}" >/dev/null
 	else
 		echo "Skipping plugin ${filename}; already downloaded."
