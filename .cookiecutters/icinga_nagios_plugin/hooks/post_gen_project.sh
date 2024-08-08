44c8a488c15b2d9346ba491c868a5ba206a01703
Christopher Peterson <3893680+cspeterson@users.noreply.github.com>
2024-08-08 15:48:44 -0400

    Cookiecutters: for icinga plugin post hook

--- .cookiecutters/icinga_nagios_plugin/hooks/post_gen_project.sh
+++ .cookiecutters/icinga_nagios_plugin/hooks/post_gen_project.sh
@@ -3,4 +3,4 @@
 # When deploying from local disk it ignores the `.gitignore` in the project
 # directory? Whatever, here is a workaround
 
-mv "{{cookiecutter.project_name}}/gitignore" "{{cookiecutter.project_name}}/.gitignore"
+mv gitignore .gitignore
